ef4af919120f9dd511ba4cfbc6847a6ec994386b
davidoska <dav.ostrovsky@gmail.com>
2019-01-05 23:17:26 +0200

    false

--- model.js
+++ model.js
@@ -1,9 +1,9 @@
-menu.onclick = function myFunction() {
-	var x = document.getElementById('MyTopnav');
+// menu.onclick = function myFunction() {
+// 	var x = document.getElementById('MyTopnav');
 
-	if (x.className === "topnav") {
-		x.className += " responsive";
-	} else {
-		x.className = "topnav";
-	}
-}
+// 	if (x.className === "topnav") {
+// 		x.className += " responsive";
+// 	} else {
+// 		x.className = "topnav";
+// 	}
+// }
